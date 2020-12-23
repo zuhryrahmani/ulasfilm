4ac9e1edc40e459f2e1071e67851b38226b7425e
Zuhry Abdi Rahmani <zuhryabdirahmani@gmail.com>
2020-12-23 09:35:19 +0700

    update

--- src/Components/NavbarHome.js
+++ src/Components/NavbarHome.js
@@ -4,7 +4,9 @@ function NavbarHome() {
   return (
     <div className='logo'>
       {/* JSX */}
-      <h1><i class="fas fa-film"></i> ReviewYuk</h1>
+      <h1 style={{fontFamily: 'Comfortaa, cursive'}}><i class="fas fa-film"></i> ReviewYuk</h1>
+      <h1 style={{fontFamily: 'Bebas Neue, cursive'}}><i class="fas fa-ticket-alt"></i> ReviewYuk</h1>
+      <h1 style={{fontFamily: 'Viga, sans-serif'}}><i class="fas fa-tape"></i> ReviewYuk</h1>
     </div>
   );
 }
